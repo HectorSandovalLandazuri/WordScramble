819cdd7e2d0ad392de177d692196619aa80cf405
Héctor Sandoval <ingsan@live.com.mx>
2023-10-30 12:15:56 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
